b63c9934068863d36fd73c58364dc4fc2f0b232b
Finik <aslamuafina@gmail.ru>
2026-02-02 20:00:23 +0300

    feat(todo): стилизация карточки задачи

--- task.js
+++ task.js
@@ -16,3 +16,26 @@ if(tasks === 0){
 }else{
     console.log("Много задач")
 }
+
+function sum(a, b){
+    return a + b
+}
+
+console.log(sum(3, 4));
+console.log(sum(10, 5));
+
+
+function isTaskDone(status){
+    return status === "выполнена"
+}
+
+console.log(isTaskDone("выполнена"));
+console.log(isTaskDone("активна"));
+
+function taskSummary(total, done){
+    let active = total - done
+    return "Всего" + total + " | Выполнено " + done + " | Активных " + active
+}
+
+console.log(taskSummary(5,3));
+console.log(taskSummary(10, 4));
